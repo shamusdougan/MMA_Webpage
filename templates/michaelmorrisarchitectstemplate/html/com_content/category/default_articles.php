cebf0c3f3f87412f4b206304fa397bc93969659c
shamusdougan <shamus@douganfamily.com.au>
2015-04-10 16:06:34 +1000

    Started the pagination for article listings

--- templates/michaelmorrisarchitectstemplate/html/com_content/category/default_articles.php
+++ templates/michaelmorrisarchitectstemplate/html/com_content/category/default_articles.php
@@ -15,70 +15,82 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 $params		= &$this->item->params;
 $n			= count($this->items);
 
-
+$jinput = JFactory::getApplication()->input;
+require_once ('/templates/michaelmorrisarchitectstemplate/library.php');
 $limit = 6;
-$offset = 0;
-jimport('joomla.html.pagination'); 
-$pageNav = new JPagination( $this->items, $offset, $limit );
+$offset = $jinput->get('offset', 0, "INT" );;
 
 
 
-$footer = $pageNav->getPaginationPages() ; 
+
 ?>
 <div class='article_listing'>
-<?php foreach ($this->items as $i => $article) 
+<?php 
+$iCount = 0;
+foreach ($this->items as $i => $article) 
 {
-	$displayArticle =& JTable::getInstance("content"); 
-	$displayArticle->load($article->id); 
-	$images = json_decode($displayArticle->images); 
-	$displayTitle = strtoupper(trim(strip_tags($displayArticle->get("title"))));
-	$displayText = trim(strip_tags($displayArticle->get("introtext")));
-	
-	$id = $article->id;
-	$alias = $displayArticle->get('alias');
-	
-	
-	if (isset($images->image_intro) and !empty($images->image_intro))
-	{
-		$displayImage = $images->image_intro;
-	}	
-	else
-	{
-		$displayImage = "images/no_image_found.jpg";
-	}
-	?>
-	
-	
-	<div class='article_listing_wrapper'>
-		<div class='article_listing_picture'>
-		 	<div class="view view-first">
-				<img src='<?php echo htmlspecialchars ("/".$displayImage); ?>' />
-			 	<div class="mask">
-	                <h2><?php echo $displayTitle; ?></h2>
-	                <p><?php echo $displayText; ?></p>
-	                <a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>" class="info">View More</a>
-	            </div>	
-			</div> 
+	if($i >= $offset && $iCount < $limit){
+		$displayArticle =& JTable::getInstance("content"); 
+		$displayArticle->load($article->id); 
+		$images = json_decode($displayArticle->images); 
+		$displayTitle = strtoupper(trim(strip_tags($displayArticle->get("title"))));
+		$displayText = trim(strip_tags($displayArticle->get("introtext")));
+		
+		$id = $article->id;
+		$alias = $displayArticle->get('alias');
+		
+		
+		if (isset($images->image_intro) and !empty($images->image_intro))
+		{
+			$displayImage = $images->image_intro;
+		}	
+		else
+		{
+			$displayImage = "images/no_image_found.jpg";
+		}
+		?>
+		
+		
+		<div class='article_listing_wrapper'>
+			<div class='article_listing_picture'>
+			 	<div class="view view-first">
+					<img src='<?php echo htmlspecialchars ("/".$displayImage); ?>' />
+				 	<div class="mask">
+		                <h2><?php echo $displayTitle; ?></h2>
+		                <p><?php echo $displayText; ?></p>
+		                <a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>" class="info">View More</a>
+		            </div>	
+				</div> 
+				
+			</div>
+			<div class='article_listing_title'>
+				<A href='<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>'>
+					<?php echo $displayTitle."(".$i.")"; ?>
+				</A>
+			</div>
+			
 			
 		</div>
-		<div class='article_listing_title'>
-			<A href='<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>'>
-				<?php echo $displayTitle; ?>
-			</A>
+		
+	
+	
+	
+<? 		$iCount++;
+		} 
+	
+
+	} ?>
+
+
+	<?php if($n != 0 && $n > $limit) { ?>
+		<div class='article_listing_footer_wrapper'>
+			<div class='article_listing_footer_content'>
+				<?php 
+				echo  sap_pagination($this->items, $offset, $limit, JRoute::_(ContentHelperRoute::getCategoryRoute($article->catid))); ?>
+			</div>
 		</div>
-		
-		
-	</div>
+	<?php } ?>
 	
-	
-	
-	
-<? } ?>
-	<div style='height: 50px; padding-top: 50px; float: left;'>
-		<?php echo $footer; ?>	
-	</div>
-
-
 </div>
 	
 
--- templates/michaelmorrisarchitectstemplate/library.php
+++ templates/michaelmorrisarchitectstemplate/library.php
@@ -17,6 +17,37 @@
 	
  }
  
+ function sap_pagination($itemList, $offset, $limit, $baseURL){
+ 	
+ 	//work out the pages number that are going to be required
+ 	$itemCount = count($itemList);
+ 	
+ 	//Work out the page count for the display
+ 	if($itemCount < $limit )
+ 	{
+		$pageCount = 1;
+	}
+	elseif($itemCount == $limit){
+		$pageCount = 1;
+	}
+	else{
+		$pageCount = $itemCount / $limit;
+		if($itemCount % $limit > 0 ){
+			$pageCount++;
+		}
+	}
+ 	
+ 	$returnString = " < ";
+ 	for($count = 1; $count <= $pageCount; $count++)
+ 	{
+		$returnString .= " ".$count." ";
+	}
+ 	$returnString .= " > ";
+ 	
+ 	
  
+ 	
+ 	return $returnString;
+ }
  
  ?> 
