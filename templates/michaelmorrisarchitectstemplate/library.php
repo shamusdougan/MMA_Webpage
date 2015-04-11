2ef981f58ab0cd17bddb31094779293fb0accffc
shamusdougan <shamus@douganfamily.com.au>
2015-04-12 08:15:59 +1000

    further works on pagination

--- templates/michaelmorrisarchitectstemplate/library.php
+++ templates/michaelmorrisarchitectstemplate/library.php
@@ -37,12 +37,40 @@
 		}
 	}
  	
- 	$returnString = " < ";
+ 	//If we are pass the first page active the prev arrow
+ 	if($offset > $limit){
+ 		$prevString = "<li><a class='hasTooltip' title='Previous' href='#'><i class='icon-previous'></i></a></li>";
+ 		} 
+ 	//if we are still in the first page
+ 	else{
+		$prevString = "<li class='sap_disabled'><a class='hasTooltip' title='Previous'><i class='icon-previous'></i></a></li>";
+	}
+ 		
+ 		
+ 	//if we are in the last page of the list then
+ 	if($offset >= ($itemCount - $limit))
+ 	{
+		$nextString = '<li class="disabled" ><a class="hasTooltip" title="Next"><i class="icon-next"></i></a></li></ul>';
+	}
+	else
+	{
+		$nextString = '<li><a class="hasTooltip" title="Next" href="#"><i class="icon-next"></i></a></li></ul>';
+	}
+ 	
+ 	$currentPage = ($offset / $limit) +1 ;
+ 	
+ 	$returnString = "<div class='pagination pagination-toolbar'><ul class='pagination-list'>".$prevString;
  	for($count = 1; $count <= $pageCount; $count++)
  	{
-		$returnString .= " ".$count." ";
+ 		$pageLinkClass = "";
+ 		if($count == $currentPage)
+ 		{
+			$pageLinkClass = "class='active'";
+		}
+ 		
+		$returnString .= "<li ".$pageLinkClass."><a>".$count."</a></li>";
 	}
- 	$returnString .= " > ";
+ 	$returnString .= $nextString;
  	
  	
  
